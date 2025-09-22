5ea3dc9a637c64ed9e21d0ec0673c41a2d6508f1
Lakshya Gupta <lakshya.gupta@adglobal360.in>
2025-09-22 23:31:05 +0530

    Created a basic structure for creating a url shortner microservice also created some congif files

---
+++ auth_service/src/config/db.config.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const databaseConnect = mongoose.connect(`${process.env.MONGO_URI}`)
+    .then(() => {
+        console.log("Mongo db connected successfully ✅")
+    })
+    .catch((error) => {
+        console.log("Error in connecting to mongo db", error)
+    })
---
+++ auth_service/src/index.js
@@ -0,0 +1,15 @@
+import { app } from "./app.js";
+import { databaseConnect } from "./config/db.config.js";
+
+const port = process.env.PORT || 3000;
+
+// connect to db
+databaseConnect;
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Running okay"})
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
