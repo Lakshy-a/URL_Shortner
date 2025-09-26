eb3e63b34d1e80c02bc6569f6d17f7062bb23111
Lakshya Gupta <lakshya.gupta@adglobal360.in>
2025-09-26 22:26:30 +0530

    Created login route

--- auth_service/src/routes/user.roues.js
+++ auth_service/src/routes/user.roues.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { register } from '../controllers/auth.controllers.js'
-import { registerValidator } from '../validators/auth.validators.js'
+import { login, register } from '../controllers/auth.controllers.js'
+import { loginValidator, registerValidator } from '../validators/auth.validators.js'
 import { validate } from '../middlewares/validate.middleware.js'
 
 const router = express.Router()
 
 router.post('/register', registerValidator, validate, register)
+router.post('/login', loginValidator, validate, login)
 
 export default router
--- auth_service/src/validators/auth.validators.js
+++ auth_service/src/validators/auth.validators.js
@@ -13,3 +13,11 @@ export const registerValidator = [
         .isLength({ min: 6 })
         .withMessage('Password must be at least 6 characters long'),
 ]
+
+export const loginValidator = [
+    body('email').isEmail().withMessage('Valid email is required'),
+
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+]
